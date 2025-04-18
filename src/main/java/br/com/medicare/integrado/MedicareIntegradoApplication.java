a7c54888e298713b1a8abdcba3f682e0f3ddbc52
Bruno Mano <brunomano2004@gmail.com>
2025-04-18 18:01:20 -0300

    Adicionando estrutura do projeto

---
+++ src/main/java/br/com/medicare/integrado/MedicareIntegradoApplication.java
@@ -0,0 +1,13 @@
+package br.com.medicare.integrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareIntegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareIntegradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/medicare/integrado/MedicareIntegradoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.medicare.integrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareIntegradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
